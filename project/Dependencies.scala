6c195eeb7e320ed5151a466f2797e632fe2582bc
Josh Suereth <joshua.suereth@gmail.com>
2014-04-29 10:41:16 -0400

    Bump to working version of sbt-remote-control for dev mode.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val sbtSnapshotVersion = "0.13.0"
   val luceneVersion = "4.2.1"
   val templateCacheVersion = "0.3.0"
-  val sbtRcVersion = "1.0-6f4f1b986c53828ab6564e5e6380bf325b275d77"
+  val sbtRcVersion = "1.0-687cea25e934e60bd9666a5de97f16ba31df86ce"
 
   val playVersion = "2.2.1"
   val webJarsVersion = "2.2.1"
