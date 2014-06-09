791c39c739c5783ca1915281337b89f484060b9c
Havoc Pennington <hp@pobox.com>
2014-06-09 11:08:59 -0400

    add observable lists of executions and tasks

--- ui/app/assets/services/sbt.js
+++ ui/app/assets/services/sbt.js
@@ -31,21 +31,106 @@ define(['commons/streams', 'commons/events', 'commons/utils'], function(streams,
     });
   }
 
+  var executionsById = {};
+  var executions = ko.observable([]);
+  var tasksById = {};
+  var tasks = ko.observable([]);
+
   var legacyLogHandlers = [];
   function legacySubscribeLog(handler) {
     legacyLogHandlers.push(handler);
   }
 
-  function eventHandler(obj) {
-    console.log("sbt event " + obj.subType, obj.event);
-    if (obj.subType == 'LogEvent') {
-      // forward legacy log event TODO this is just a demo hack
-      $.each(legacyLogHandlers, function(index, subscriber) {
-          subscriber({
-            'type' : 'LogEvent',
-            'entry' : obj.event.entry
-          });
+  function removeExecution(id, succeeded) {
+    var execution = executionsById[id];
+    if (execution) {
+      executions.remove(function(item) {
+        return item.executionId == execution.executionId;
       });
+      // we want succeeded flag up-to-date when finished notifies
+      execution.succeeded(event.success);
+      execution.finished(true);
+      delete executionsById[execution.executionId];
+    }
+  }
+
+  var subTypeHandlers = {
+      LogEvent: function(event) {
+        // forward legacy log event TODO this is just a demo hack
+        $.each(legacyLogHandlers, function(index, subscriber) {
+            subscriber({
+              'type' : 'LogEvent',
+              'entry' : event.entry
+            });
+        });
+      },
+      TaskStarted: function(event) {
+        debug && console.log("TaskStarted ", event);
+        var task = {
+            execution: executionsById[event.executionId],
+            taskId: event.taskId,
+            key: event.key ? event.key.key.name : null,
+            finished: ko.observable(false),
+            succeeded: ko.observable(false)
+        };
+        console.log("Starting task ", task);
+        // we want to be in the by-id hash before we notify
+        // on the tasks array
+        tasksById[task.taskId] = task;
+        tasks.push(task);
+      },
+      TaskFinished: function(event) {
+        debug && console.log("TaskFinished ", event);
+        var task = tasksById[event.taskId];
+        if (task) {
+          tasks.remove(function(item) {
+            return item.taskId == task.taskId;
+          });
+          // we want succeeded flag up-to-date when finished notifies
+          task.succeeded(event.success);
+          task.finished(true);
+          delete tasksById[task.taskId];
+        }
+      },
+      ExecutionWaiting: function(event) {
+        debug && console.log("ExecutionWaiting ", event);
+        // event.id
+        // event.command
+      },
+      ExecutionStarting: function(event) {
+        debug && console.log("ExecutionStarting ", event);
+        var execution = {
+            executionId: event.id,
+            finished: ko.observable(false),
+            succeeded: ko.observable(false)
+        };
+        // we want to be in the by-id hash before we notify
+        // on the executions array
+        executionsById[execution.executionId] = execution;
+        executions.push(execution);
+      },
+      ExecutionFailure: function(event) {
+        debug && console.log("ExecutionFailure ", event);
+        removeExecution(event.id, false /* succeeded */);
+      },
+      ExecutionSuccess: function(event) {
+        debug && console.log("ExecutionSuccess ", event);
+        removeExecution(event.id, true /* succeeded */);
+      },
+      CompilationFailure: function(event) {
+        debug && console.log("CompilationFailure ", event);
+      },
+      TestEvent: function(event) {
+        debug && console.log("TestEvent ", event);
+      }
+  }
+
+  function eventHandler(obj) {
+    debug && console.log("sbt event " + obj.subType, obj.event);
+    if (obj.subType in subTypeHandlers) {
+      subTypeHandlers[obj.subType](obj.event);
+    } else {
+      console.log("not handling sbt event of type " + obj.subType, obj.event);
     }
   }
 
@@ -58,11 +143,11 @@ define(['commons/streams', 'commons/events', 'commons/utils'], function(streams,
     handler: eventHandler
   });
 
-  // TODO do something with the events (like track a model or stream of tasks and logs)
-
   return {
     possibleAutocompletions: possibleAutocompletions,
     requestExecution: requestExecution,
-    legacySubscribeLog: legacySubscribeLog
+    legacySubscribeLog: legacySubscribeLog,
+    executions: executions,
+    tasks: tasks
   };
 })
